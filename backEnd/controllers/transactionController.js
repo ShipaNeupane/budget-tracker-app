f820fc2d2b662f51038c5e4c106df91b05a7a25e
Shipa Neupane <shipaneupane2@gmail.com>
2024-09-20 04:58:36 -0400

    adding few validation for fields

--- backEnd/controllers/transactionController.js
+++ backEnd/controllers/transactionController.js
@@ -1,15 +1,32 @@
 import { addTransaction, getTransactions } from '../models/transactionModel.js';
 
 export const createTransaction = (req, res) => {
-    const transaction = req.body;
-    addTransaction(transaction, (err) => {
-        if (err) {
+    const { amount, date, category, type } = req.body;
+
+    // Ensure required fields are present
+    if (!amount || !date || !category || !type) {
+    return res.status(400).json({
+        message: 'Missing required fields: amount, date, category, or type',
+    });
+    }
+
+    // Check if amount is a valid number
+    if (isNaN(amount)) {
+    return res.status(400).json({
+        message: 'Amount must be a valid number',
+    });
+    }
+
+    // If validation passes, add the transaction to the DB
+    addTransaction(req.body, (err) => {
+    if (err) {
         res.status(500).json({ message: 'Error adding transaction' });
-        } else {
+    } else {
         res.status(201).json({ message: 'Transaction added successfully' });
-        }
+    }
     });
 };
+
 
 export const getAllTransactions = (req, res) => {
     getTransactions((err, rows) => {
